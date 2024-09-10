442fe95a7b7d660a465db85b8ffd61e438a51449
Vijay K <vijay.k@codecraft.co.io>
2024-09-10 17:21:51 +0530

    Initial Commit

---
+++ JobDashboard/JobDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobDashboardApp.swift
+//  JobDashboard
+//
+//  Created by vijay on 10/09/24.
+//
+
+import SwiftUI
+
+@main
+struct JobDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
